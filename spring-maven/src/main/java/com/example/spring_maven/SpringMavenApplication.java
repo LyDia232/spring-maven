da3eb4217c0cd9eadec2267a44d098e59d11db8f
LyDia232 <lydia.akmoussi7@gmail.com>
2026-06-29 12:14:37 +0200

    Add GitHub Actions CI/CD pipeline

--- spring-maven/src/main/java/com/example/spring_maven/SpringMavenApplication.java
+++ spring-maven/src/main/java/com/example/spring_maven/SpringMavenApplication.java
@@ -2,12 +2,24 @@ package com.example.spring_maven;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class SpringMavenApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringMavenApplication.class, args);
+	}
+
+}
+
+@RestController
+class HelloWorldController {
+
+	@GetMapping("/")
+	public String sayHello() {
+		return "Hello, World!";
 	}
 
 }
